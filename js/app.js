dde5c88076fe922fc66467cf3fd2ab86202501a2
MilagrosQB <qbmilagros@gmail.com>
2016-11-08 20:57:42 -0500

    ultimos cambios del dia

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 $(window).load(function() {
 	$(".loading").fadeOut("slow");
 });
+
+
+$('input[name="number"]').keyup(function(e)
+                                {
+  if (/\D/g.test(this.value))
+  {
+    this.value = this.value.replace(/\D/g, '');
+  }
+});
