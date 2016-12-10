a4e282bf61b9b9f5a73b2b6841666b503e07e595
MilagrosQB <qbmilagros@gmail.com>
2016-12-10 01:25:17 -0500

    search

--- js/map.js
+++ js/map.js
@@ -5,29 +5,51 @@ function initMap() {
   });
   var infoWindow = new google.maps.InfoWindow({map: map});
 
+    var geocoder = new google.maps.Geocoder();
+
+  document.getElementById('pickup').addEventListener('click', function() {
+    geocodeAddress(geocoder, map);
+  });
+
   // Try HTML5 geolocation.
   if (navigator.geolocation) {
-  	navigator.geolocation.getCurrentPosition(function(position) {
-  		var pos = {
-  			lat: position.coords.latitude,
-  			lng: position.coords.longitude
-  		};
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
 
-  		infoWindow.setPosition(pos);
-  		infoWindow.setContent('You are here.');
-  		map.setCenter(pos);
-  	}, function() {
-  		handleLocationError(true, infoWindow, map.getCenter());
-  	});
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('You are here.');
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
   } else {
     // Browser doesn't support Geolocation
     handleLocationError(false, infoWindow, map.getCenter());
   }
-}
+  }
+
+function geocodeAddress(geocoder, resultsMap) {
+  var address = document.getElementById('address').value;
+  geocoder.geocode({'address': address}, function(results, status) {
+    if (status === google.maps.GeocoderStatus.OK) {
+      resultsMap.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+        map: resultsMap,
+        position: results[0].geometry.location
+      });
+
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+    };
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   infoWindow.setPosition(pos);
   infoWindow.setContent(browserHasGeolocation ?
         'Error: The Geolocation service failed.' :
-		'Error: Your browser doesn\'t support geolocation.');
-}
+    'Error: Your browser doesn\'t support geolocation.');
+}    
